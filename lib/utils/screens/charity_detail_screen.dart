ea90e1b463d79abcbeddc95b8dd81bd835e66155
Monkey 27 <terryrees@hotmail.com>
2019-08-22 22:34:51 +0100

    Final update before submission

--- lib/utils/screens/charity_detail_screen.dart
+++ lib/utils/screens/charity_detail_screen.dart
@@ -1,55 +1,54 @@
 import 'package:flutter/material.dart';
 
 class CharityDetailScreen extends StatelessWidget {
-//  final document;
+  final selectedCharityObject;
 
-//  CharityDetailScreen(this.document);
+  CharityDetailScreen(this.selectedCharityObject);
+
+
 
   @override
   Widget build(BuildContext context) {
+    String _charityName = selectedCharityObject["charityName"];
+    String _charityDescription = selectedCharityObject["charityDescription"];
+    String _charityAddress1 = selectedCharityObject["charityAddress1"];
+    String _charityAddress2 = selectedCharityObject["charityAddress2"];
+    String _charityTown = selectedCharityObject["charityTown"];
+    String _charityCounty = selectedCharityObject["charityCounty"];
+    String _charityPostcode = selectedCharityObject["charityPostcode"];
+    String _charityContactName = selectedCharityObject["charityContactName"];
+    String _charityTelephone = selectedCharityObject["charityTelephone"];
+    String _charityMail = selectedCharityObject["charityMail"];
     return Scaffold(
-        appBar: AppBar(title: Text("Charity Name")),
+        appBar: AppBar(title: Text(_charityName)),
         body: SingleChildScrollView(
           child: Padding(
               padding: const EdgeInsets.only(top: 16.0, left: 32, right: 32),
               child: Column(children: <Widget>[
-                Text("Charity Description"),
                 Text("CONTACT:"),
                 Row(
                   children: <Widget>[
                     Column(
                       children: <Widget>[
-                        Text("Address 1"),
-                        Text("Address 2"),
-                        Text("Town"),
-                        Text("County"),
-                        Text("Postcode"),
+                        Text(_charityAddress1),
+                        Text(_charityAddress2),
+                        Text(_charityTown),
+                        Text(_charityCounty),
+                        Text(_charityPostcode),
                       ],
                     ),
                     Column(
                       children: <Widget>[
-                        Text("Contact"),
-                        Text("Name"),
-                        Text("Telephone"),
-                        Text("email"),
+                        Text("Contact:"),
+                        Text(_charityContactName),
+                        Text(_charityTelephone),
+                        Text(_charityMail),
                       ],
                     )
                   ],
                 ),
-                Text("Charity Description"),
-                Text("Charity Description"),
-                Text("Charity Description"),
-                Text("Charity Description"),
-                Text("Charity Description"),
-                Text("Charity Description"),
-                Text("Charity Description"),
-                Text("Charity Description"),
+                Text(_charityDescription),
               ])),
         ));
   }
-
-//  String _auctionStatus() {
-//    return "Difference";
-//  }
-
 }
